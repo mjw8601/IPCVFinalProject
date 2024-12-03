9c1b6bb101b837cc229a653ca400a451a944210e
Gian-Mateo <64568308+AntiPersnlMyne@users.noreply.github.com>
2024-12-03 12:48:33 -0500

    Better Segmentation
    Added statistical analysis & ratio analysis (versus bounding-box) for contours, to better segment characters from license plates.
    From preliminary tests, auto_extract_characters() finds *at least* all characters from a license plate. 
    auto_extract_characters() will also pick up noisy characters, but these have been greatly reduced.

--- knnconcept.cpp
+++ knnconcept.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
@@ -8,12 +9,7 @@ using namespace std;
 
 // Functions - written below main
 void ColorReduce(const cv::Mat& src, cv::Mat& dst, const int& div=64);
-void MouseHandler(int event, int x, int y, int flags, void* userdata);
 std::vector<cv::Mat> auto_extract_characters(cv::Mat& license_plate);
-
-// Points for manual segmentation of characters
-std::vector<cv::Point2f> src_points (4);
-std::vector<cv::Point2f> dst_points (4);
 
 int main(int argc, char* argv[]) {
     // ##########################
@@ -42,48 +38,6 @@ int main(int argc, char* argv[]) {
     // cv::waitKey(0);
 
 
-    // ##############################
-    // ###### Draw Box Testing ######
-    // ##############################
-
-    // // Read in an image
-    // cv::Mat src_image = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
-    // cv::Mat dst_image;
-
-    // // Fit image to the screen and maintain image ratio
-    // cv::namedWindow("Select Points", cv::WINDOW_KEEPRATIO);
-    // cv::setWindowProperty("Select Points", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
-    // // Show image to crop
-    // cv::imshow("Select Points", src_image);
-    // cv::setMouseCallback("Select Points", MouseHandler);
-
-    // // Wait to select 4 points
-    // while (src_points.size() != 4) {
-    //     cv::waitKey(1);
-    // }
-
-    // // Define letter box size (28x28)
-    // // However, I could make this rectilinear, 
-    // // I have no arguement for one or the other
-    // dst_points = {
-    //     cv::Point2f(0, 0),    // Top-left corner
-    //     cv::Point2f(28, 0),   // Top-right corner
-    //     cv::Point2f(28, 28),  // Bottom-right corner
-    //     cv::Point2f(0, 28)    // Bottom-left corner
-    // };
-
-    //     // Calculate the perspective transformation matrix
-    // cv::Mat perspective_matrix = cv::getPerspectiveTransform(src_points, dst_points);
-
-    // // Perform the perspective warp
-    // cv::Mat perspective_image;
-    // cv::warpPerspective(src_image, perspective_image, perspective_matrix, src_image.size());
-
-    // // Display the output image
-    // cv::namedWindow("Warped Image", cv::WINDOW_NORMAL);
-    // cv::imshow("Warped Image", perspective_image);
-    // cv::waitKey(0);
-
     // ######################################
     // %% Automatic Character Segmentation %%
     // ######################################
@@ -98,7 +52,6 @@ int main(int argc, char* argv[]) {
     for (size_t i = 0; i < characters_in_plate.size(); ++i) {
         try {
         cv::imshow("Character " + std::to_string(i), characters_in_plate[i]);
-        cv::imwrite("character_" + std::to_string(i) + ".jpg", characters_in_plate[i]);
         } 
         catch (...) { // Catch any errors in displaying empty matrices
             cerr << i << " is where we crashed" << endl;
@@ -142,75 +95,6 @@ void ColorReduce(const cv::Mat& src, cv::Mat& dst, const int& div)
 }
 
 /**
- * \brief Handles mouse inputs for clicking 4 points to create quadralateral
- * 
- * \param[in] event Listens to mouse events (i.e. left click, right click, etc.)
- * \param[in] x Horizontal coordinate of the click
- * \param[in] y Vertical coordinate of the click
- * \param[in] flags (Optional) User flags (N/A)
- * \param[in] userdata (Optional) Reference to user data (N/A)
- */
-void MouseHandler(int event, int x, int y, int flags, void* userdata) {
-    if (event == cv::EVENT_LBUTTONDOWN) {
-        // Record the point
-        src_points.push_back(cv::Point2d(x, y));
-        std::cout << "Point selected: (" << x << ", " << y << ")\n";
-
-        // If 4 points are selected, close the window
-        if (src_points.size() == 4) {
-            std::cout << "All 4 points selected!\n";
-        }
-    }
-}
-
-/**
- * \brief Crops (isolates) letter from license plate image 
- * 
- * \param[in] license_plate Image to perform q2q
- * \param[out] src_points 
- * \param[out] dst_points 
- * \return cv::Mat Isolated character from license plate
- */
-std::vector<cv::Mat> extract_characters(
-        const cv::Mat& license_plate, 
-        std::vector<cv::Point2f>& src_points, 
-        std::vector<cv::Point2f>& dst_points) {
-    // Clear srcPoints for new image
-    src_points.clear();
-
-    // Show the image and set up the callback
-    cv::imshow("Select Points", license_plate);
-    cv::setMouseCallback("Select Points", MouseHandler);
-
-    // Wait for the user to select 4 points
-    while (src_points.size() < 4) {
-        cv::waitKey(1);
-    }
-
-    // Define the destination quadrilateral
-    std::vector<cv::Point2f> dstPoints = {
-        cv::Point2f(100, 100),
-        cv::Point2f(500, 100),
-        cv::Point2f(500, 400),
-        cv::Point2f(100, 400)
-    };
-
-    // Calculate the perspective transformation matrix
-    cv::Mat perspectiveMatrix = cv::getPerspectiveTransform(src_points, dstPoints);
-
-    // Perform the perspective warp
-    cv::Mat outputImage;
-    cv::warpPerspective(license_plate, outputImage, perspectiveMatrix, license_plate.size());
-
-    // Display the output
-    cv::imshow("Warped Image", outputImage);
-    cv::waitKey(0);
-
-    // Clean up
-    cv::destroyAllWindows();
-}
-
-/**
  * \brief Automatically finds and extracts characters from license plates
  * 
  * \param[in] license_plate Grayscale license plate photo from parking lot / Grayscale license plate from isolate function
@@ -223,12 +107,8 @@ std::vector<cv::Mat> auto_extract_characters(cv::Mat& license_plate) {
 
     // Threshold image to increase contrast
     cv::GaussianBlur(license_plate, license_plate, cv::Size(5, 5), 0); // (Optional) Improves threshold results
-    cv::threshold(license_plate, license_plate, 80, 255,  cv::THRESH_BINARY);
+    cv::threshold(license_plate, license_plate, 80, 255,  cv::THRESH_BINARY | cv::THRESH_OTSU);
     
-    /* This, in my tests, produces worse results. Reason being is that the letters are always black, and the background always isn't. 
-       So, choosing a color near black works for letters in the plate itself, and the bounding box takes care of whatever else is out there */
-    // cv::threshold(license_plate, license_plate, 100, 255,  cv::THRESH_OTSU | cv::THRESH_BINARY); 
-
 
     // ###################
     // %% Find Contours %%
@@ -240,9 +120,12 @@ std::vector<cv::Mat> auto_extract_characters(cv::Mat& license_plate) {
     std::vector<std::vector<cv::Point>> contours;
     cv::Mat license_copy = license_plate.clone();
 
+    // cv::imshow("plate", license_copy);
+    // cv::waitKey(0);
+
     // Find contours of the binary image
-    // Most simple, and fastest implementation
-    cv::findContours(license_copy, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+    // RETR_TREE, RETR_LIST, RETR_CCOMP
+    cv::findContours(license_copy, contours, cv::RETR_TREE , cv::CHAIN_APPROX_NONE);
 
 
     // ########################
@@ -255,9 +138,10 @@ std::vector<cv::Mat> auto_extract_characters(cv::Mat& license_plate) {
         cv::Rect bounding_box = cv::boundingRect(contour);
 
         // Find characters based on size of their contours
-        if (bounding_box.width > 100 && bounding_box.width < 300 
-        && bounding_box.height > 200 && bounding_box.height < 500) { // (Adjustable) threshold values
-        cout << bounding_box.size() << endl;
+        if (bounding_box.width > 100 && bounding_box.width < 500 
+        && bounding_box.height > 50 && bounding_box.height < 700) { // (Adjustable) threshold values
+        cout << "Bounding size: " << bounding_box.size() << endl;
+
             
             cv::Mat character = license_plate(bounding_box);
 
@@ -275,53 +159,6 @@ std::vector<cv::Mat> auto_extract_characters(cv::Mat& license_plate) {
     return characters; // vector<Mat>
 
 }
-
-/**
- * \brief Isolates license plate from image assuming common ratios
- * 
- * \param[in] image Grayscale parking-lot image
- * \return cv::Mat 
- */
-cv::Mat isolateLicensePlate(const cv::Mat& image) {
-    cv::Mat gray, edges, license_plate;
-    
-    // Resize image for consistent plate size detection
-    cv::Mat resized;
-    cv::resize(image, resized, cv::Size(1000, 750));
-    
-    // Convert to grayscale
-    cv::cvtColor(resized, gray, cv::COLOR_BGR2GRAY);
-    
-    // Apply GaussianBlur to reduce noise
-    cv::GaussianBlur(gray, gray, cv::Size(5, 5), 0);
-    
-    // Edge detection
-    cv::Canny(gray, edges, 100, 200);
-
-    // Find contours
-    std::vector<std::vector<cv::Point>> contours;
-    cv::findContours(edges, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
-    // Filter contours to find license plate
-    for (const auto& contour : contours) {
-        cv::Rect bounding_box = cv::boundingRect(contour);
-        
-        // Filter based on aspect ratio and size
-        double aspect_ratio = (double)bounding_box.width / bounding_box.height;
-        if (aspect_ratio > 2.0 && aspect_ratio < 6.0 && // Typical license plate aspect ratio
-            bounding_box.width > 200 && bounding_box.width < 1000 && // Width range
-            bounding_box.height > 50 && bounding_box.height < 300) { // Height range
-            
-            // Extract the license plate region
-            license_plate = resized(bounding_box);
-            break;
-        }
-    }
-
-    return license_plate;
-}
-
-
 
 
 
